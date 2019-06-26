af279e55ff71fada16b2bb39ffd96be16407dea0
ekta saraogi <ekta.sg@gmail.com>
2019-06-27 01:13:52 +0530

    Add author and reviews model, link book to author, update admin, create book detail page modifying url, view and template.

--- store/urls.py
+++ store/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^book/(\d+)', views.book_details, name='book_details'),
     url(r'^', views.store, name='index'),
 ]
