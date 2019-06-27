9f94d8359efcd50fa09ed03c1d44c96173cd97ee
ekta saraogi <ekta.sg@gmail.com>
2019-06-28 00:00:37 +0530

    Added Shopping cart - Added models Cart and BookOrder and added views, urls and templates to add and remove from cart.

--- store/admin.py
+++ store/admin.py
@@ -1,16 +1,26 @@
 from django.contrib import admin
-from .models import Book,Author
+from .models import Book, Author, Cart, BookOrder
 
 # Register your models here.
 
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author' , 'price','stock')
+    list_display = ('title', 'author', 'price','stock')
 
 
 class AuthorAdmin(admin.ModelAdmin):
    list_display = ('last_name', 'first_name')
 
 
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'active', 'order_date')
+
+
+class BookOrderAdmin(admin.ModelAdmin):
+    list_display = ('book', 'cart' , 'quantity')
+
+
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author, AuthorAdmin)
+admin.site.register(Cart, CartAdmin)
+admin.site.register(BookOrder, BookOrderAdmin)
